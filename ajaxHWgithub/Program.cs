cefcc3aef300dc37e329883c0bf56e63fbf97d60
eric <ericispanasp@gmail.com>
2023-01-09 22:35:08 +0800

    fetch area complete

--- ajaxHWgithub/Program.cs
+++ ajaxHWgithub/Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=fetchTest}/{action=Index}/{id?}");
 
 app.Run();
